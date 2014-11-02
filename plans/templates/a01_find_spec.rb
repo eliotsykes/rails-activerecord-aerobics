e2a450279c65151404fa5bcff06c7e14ea77521e
Eliot Sykes <eliotsykes@gmail.com>
2014-11-02 12:15:26 +0000

    Make exercise more rapid-fire drill-like

--- plans/templates/a01_find_spec.rb
+++ plans/templates/a01_find_spec.rb
@@ -2,49 +2,73 @@ require "rails_helper"
 
 describe ".find(*args) workout" do
 
-  context ".find(id) finds record by a single id" do
+  context ".find(id) finds record by a single id (aka primary key)" do
 
-    context "when the id is a number (Fixnum)" do
-
-      # Here's the first one done for you!
-      it "finds book with id 1 using Book.find(1)" do
-        create(:book, id: 1, title: "Agile Web Development with Rails")
-        
-        book = Book.find(1)
-        
-        expect(book.id).to eq(1)
-        expect(book.title).to eq("Agile Web Development with Rails")
-      end
-
-      # From now on replace YOUR_CODE_HERE with code to make the spec pass
-      it "finds book with id 2 using Book.find(2)" do
-        create(:book, id: 2, title: "Programming Ruby")
-        
-        # See the YOUR_CODE_HERE? Replace it with code to make
-        # this spec pass:
-        book = Book.find(2)#hide
-        # book = YOUR_CODE_HERE#show
-        
-        expect(book.id).to eq(2)
-        expect(book.title).to eq("Programming Ruby")
-      end
-
+    before do
+      @agile_book = create(:book, id: 1, title: "Agile Web Development with Rails")
+      @prog_book = create(:book, id: 123, title: "Programming Ruby")
+      @exceptional_book = create(:book, id: 987654, title: "Exceptional Ruby")
+      @refactor_book = create(:book, id: 99, title: "Refactoring: Ruby Edition")
+      @practice_book = create(:book, id: 11223344, title: "Practicing Rails")
     end
 
-    context "when the id is a number (String)" do
+    it "handles numeric (Fixnum) ids" do
+      # Here's the first one done for you!
+      expect(Book.find(1)).to eq(@agile_book)#hide
+      # expect(Book.find(1)).to eq(@agile_book)#show
 
-      it 'finds book with id "123" (a String, not a Fixnum)' do
-        create(:book, id: 123, title: "Exceptional Ruby")
-        
-        book = Book.find("123")#hide
-        # book = YOUR_CODE_HERE "123"#show
+      # From now on replace YOUR_CODE_HERE with code to make the spec pass
+      expect(Book.find(123)).to eq(@prog_book)#hide
+      # expect(YOUR_CODE_HERE(123)).to eq(@prog_book)#show
 
-        expect(book.id).to eq(123)
-        expect(book.title).to eq("Exceptional Ruby")
-      end
+      expect(Book.find(987654)).to eq(@exceptional_book)#hide
+      # expect(YOUR_CODE_HERE(987654)).to eq(@exceptional_book)#show
+      
+      expect(Book.find(99)).to eq(@refactor_book)#hide
+      # expect(YOUR_CODE_HERE).to eq(@refactor_book)#show
 
+      expect(Book.find(11223344)).to eq(@practice_book)#hide
+      # expect(YOUR_CODE_HERE).to eq(@practice_book)#show
+    end
+
+    it "handles pure numerical String ids" do
+      expect(Book.find("1")).to eq(@agile_book)
+
+      expect(Book.find("123")).to eq(@prog_book)#hide
+      # expect(YOUR_CODE_HERE("123")).to eq(@prog_book)#show
+
+      expect(Book.find("987654")).to eq(@exceptional_book)#hide
+      # expect(YOUR_CODE_HERE("987654")).to eq(@exceptional_book)#show
+      
+      expect(Book.find("99")).to eq(@refactor_book)#hide
+      # expect(YOUR_CODE_HERE).to eq(@refactor_book)#show
+
+      expect(Book.find("11223344")).to eq(@practice_book)#hide
+      # expect(YOUR_CODE_HERE).to eq(@practice_book)#show
+    end
+
+    it "handles Strings that can be converted to numeric ids" do
+      expect(Book.find("1-agile-web")).to eq(@agile_book)
+
+      expect(Book.find("123_ruby_prog_book")).to eq(@prog_book)#hide
+      # expect(YOUR_CODE_HERE("123_ruby_prog_book")).to eq(@prog_book)#show
+
+      expect(Book.find("987654 EXCEPTIONAL RUBY")).to eq(@exceptional_book)#hide
+      # expect(YOUR_CODE_HERE("987654 EXCEPTIONAL RUBY")).to eq(@exceptional_book)#show
+      
+      expect(Book.find("99-rred")).to eq(@refactor_book)#hide
+      # expect(YOUR_CODE_HERE("99-rred")).to eq(@refactor_book)#show
+
+      expect(Book.find("11223344 Hello World!")).to eq(@practice_book)#hide
+      # expect(YOUR_CODE_HERE("11223344 Hello World!")).to eq(@practice_book)#show
+
+      # Wondering how the String-to-number conversion happens? 
+      # The String#to_i method is used to convert the Strings into integers. 
+      # Feel free to try these out in your console (irb or 'rails c'):
+      # "1-agile-web".to_i # returns 1 (Fixnum)
+      # "123_ruby_prog_book".to_i # returns 123 (Fixnum)
     end
 
   end
-  
+
 end
