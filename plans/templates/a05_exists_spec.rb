1260b15c779237524601f6775c50fc7ed9f6901c
Eliot Sykes <eliotsykes@gmail.com>
2014-11-04 18:01:36 +0000

    Exercises for exists?(id_as_num_or_str)

--- plans/templates/a05_exists_spec.rb
+++ plans/templates/a05_exists_spec.rb
@@ -32,4 +32,39 @@ describe "exists? workout" do
 
   end
 
+  context "with Integer or String id argument" do
+
+    it "tells you if record with the given primary key exists" do
+
+      create(:meeting, id: 111)
+      expect(Meeting.exists?(111)).to eq(true)
+      expect(Meeting.exists?(111.to_s)).to eq(true)
+      expect(Meeting.exists?(222)).to eq(false)#hide
+      # expect(Meeting.exists?(222)).to eq(YOUR_CODE_HERE)#show
+      expect(Meeting.exists?(222.to_s)).to eq(false)#hide
+      # expect(Meeting.exists?(222.to_s)).to eq(YOUR_CODE_HERE)#show
+
+      create(:country, id: 9876)
+      expect(Country.exists?(9876)).to eq(true)#hide
+      # expect(Country.exists?(YOUR_CODE_HERE)).to eq(true)#show
+      expect(Country.exists?(9876.to_s)).to eq(true)#hide
+      # expect(Country.YOUR_CODE_HERE(9876.to_s)).to eq(YOUR_CODE_HERE)#show
+      expect(Country.exists?(1234)).to eq(false)#hide
+      # expect(YOUR_CODE_HERE(1234)).to eq(false)#show
+      expect(Country.exists?(1234.to_s)).to eq(false)#hide
+      # expect(YOUR_CODE_HERE(YOUR_CODE_HERE.to_s)).to eq(false)#show
+
+      create(:book, id: 1)
+      expect(Book.exists?(1)).to eq(true)#hide
+      # expect(YOUR_CODE_HERE(1)).to eq(true)#show
+      expect(Book.exists?(1.to_s)).to eq(true)#hide
+      # expect(YOUR_CODE_HERE(1.to_s)).to eq(true)#show
+      expect(Book.exists?(2)).to eq(false)#hide
+      # expect(YOUR_CODE_HERE(2)).to eq(false)#show
+      expect(Book.exists?(2.to_s)).to eq(false)#hide
+      # expect(YOUR_CODE_HERE(2.to_s)).to eq(false)#show
+    end
+
+  end
+
 end
