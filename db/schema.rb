8068a9d64da01fe0a1fd5407c393ee6bcfb2e333
Eliot Sykes <eliotsykes@gmail.com>
2014-11-04 17:14:10 +0000

    Introduce Meeting model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141102170409) do
+ActiveRecord::Schema.define(version: 20141104165803) do
 
   create_table "articles", force: true do |t|
     t.string   "headline"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 20141102170409) do
 
   create_table "countries", force: true do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "meetings", force: true do |t|
+    t.datetime "start_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
