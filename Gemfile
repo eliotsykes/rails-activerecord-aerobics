a7c2d7b026938f9d0adbcac9f09a01df24a9c14d
Eliot Sykes <eliotsykes@gmail.com>
2014-11-07 13:10:59 +0000

    Add awesome_print gem, setup Product and Category seed data for query practice

--- Gemfile
+++ Gemfile
@@ -21,8 +21,9 @@ group :development, :test do
   gem 'rspec-rails', '~> 3.1.0'
   gem 'factory_girl_rails', '~> 4.5'
   gem 'faker', '~> 1.4'
+  gem 'awesome_print', '~> 1.2.0'
 end
 
 group :test do
-  gem 'shoulda-matchers', require: false
+  gem 'shoulda-matchers', '~> 2.7.0', require: false
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,31 @@
 # the canned test data specified in this file into the db. You could also edit this file just to contain the
 # test data you want.
 
+@model_classes = [Category, Product]
+
+def delete_all
+  @model_classes.each do |model_class|
+    puts "Deleting existing data for #{model_class}"
+    model_class.delete_all
+  end
+end
+
+delete_all
+
+num_categories = 5
+num_products = num_categories * 5
+
+categories = []
+
+num_categories.times do
+  categories << Category.create!(name: Faker::Commerce.department)
+end
+
+num_products.times do
+  random_category = categories.sample
+  random_category.products.create!(name: Faker::Commerce.product_name)
+end
+
+@model_classes.each do |model_class|
+  puts "Created #{model_class.count} records for #{model_class}"
+end
